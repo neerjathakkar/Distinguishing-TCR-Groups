a6e496afbf3595064b26fd2008cc8386e601d715
neerjathakkar <neerja.b.thakkar.19@dartmouth.edu>
2019-05-05 20:28:02 -0400

    Update sw_scoring.py

--- sw_scoring.py
+++ sw_scoring.py
@@ -5,6 +5,7 @@
 # Smith Waterman scoring method
 
 import swalign
+import math
 
 # uses a gap penalty of 10
 def getScoreSW(aa1, aa2, gap_penalty=-10):
@@ -18,22 +19,18 @@ def getScoreSW(aa1, aa2, gap_penalty=-10):
     return alignment.score
 
 # returns a normalized (between 0 and 1) SW distance
+# CDRdist=   1 -  { (sw(A,B))^2   /       { sw(A,A) * sw(B,B) } }
 def getDistanceSW(aa1, aa2, length_dep=True, gap_penalty=-10):
     if length_dep:
         if len(aa1) != len(aa2):
             return 1.0
-            
+
      # get self-scores for both
     self_score_1 = getScoreSW(aa1, aa1, gap_penalty)
     self_score_2 = getScoreSW(aa2, aa2, gap_penalty)
 
-    # we want to divide by the min distance
-    min_self_score = float(min(self_score_1, self_score_2))
+    score = getScoreSW(aa1, aa2, gap_penalty)
 
-    if min_self_score <= 0:
-        print("warning: negative or zero self-score")
-        return 1.0
+    normalized = (score * score) / (self_score_1 * self_score_2)
 
-    normalized = getScoreSW(aa1, aa2, gap_penalty)/min_self_score
-
-    return 1.0 - normalized
+    return 1.0 - math.sqrt(normalized)
